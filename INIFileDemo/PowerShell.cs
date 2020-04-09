256400b9ba556b3216d14dc9709ece2d095a50fb
Ajay Elme <40856072+ajayelme@users.noreply.github.com>
2020-04-09 17:16:37 +0530

    Add files via upload

---
+++ INIFileDemo/PowerShell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CustomActionUtility
+{
+    internal class PowerShell
+    {
+        internal static PowerShell Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void AddScript(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Invoke()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
